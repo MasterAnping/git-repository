9335fcbd84acce0b5633c76c572536c7c4215753
MasterAnping <MasterAnping@Gmail.com>
2019-08-28 11:35:22 +0800

    初始

---
+++ GitTest2/src/main/java/com/zap/GitTest2/App.java
@@ -0,0 +1,13 @@
+package com.zap.GitTest2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
